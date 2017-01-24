a70f157701bb84d2d632641bbdd9776a4e196fe7
altantur <bayarsaikhan_altantur@unimedia.co.jp>
2017-01-24 16:01:26 +0800

    Client side basic done

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080
+var path = require('path'),
+    express = require('express'),
+    app = express(),
+    createHandler = require('./server/gitlab-webhook-handler'),
+    Trello = require('node-trello'),
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    levelup = require('levelup'),
+    db = levelup('./trellodb');
+const PORT = process.env.PORT || 8080;
+const TRELLO_KEY = '606e8e43f53447a2819dd630338306aa'
+const TRELLO_TOKEN = '4c8f2d39e4174b71ad618ba605f58ddea6c36d842cd193b66154def68417871a'
+const WEBHOOK_URL = '/webhook'
+const WEBHOOK_SECRET = 'myhashsecret'
+const BASE_URL = 'http://localhost:8080'
+
+var handler = createHandler({ path: WEBHOOK_URL, secret:  WEBHOOK_SECRET})
+var t = new Trello(TRELLO_KEY, TRELLO_TOKEN)
 
 // using webpack-dev-server and middleware in development environment
 if(process.env.NODE_ENV !== 'production') {
@@ -10,15 +24,38 @@ if(process.env.NODE_ENV !== 'production') {
   var webpack = require('webpack');
   var config = require('./webpack.config');
   var compiler = webpack(config);
-  
+
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
+});
+
+app.post('/setwebhook', function(request, response) {
+  var trello = {
+    trelloToken : request.body.trelloToken,
+    boardId : request.body.boardId
+  }
+  var hashsecret = Math.floor(Date.now()/10).toString(16)
+  db.put(hashsecret, trello, function (err) {
+    if(err) return console.log("Error folks!", err);
+    db.get(hashsecret, function (err, value) {
+      if(err) return console.log("Error folks!", err);
+    })
+  })
+    response.send(BASE_URL + WEBHOOK_URL + "?hashsecret=" + hashsecret)
+});
+
+app.get(WEBHOOK_URL, function(request, response) {
+  handler(request, response, function(err){
+    response.statusCode = 404
+    response.sendFile(__dirname + '/dist/index.html')
+  });
 });
 
 app.listen(PORT, function(error) {
@@ -28,3 +65,19 @@ app.listen(PORT, function(error) {
     console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   }
 });
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('issue', function (event) {
+  //   console.log('Received an issue event for %s action=%s: #%d %s',
+  //   event.payload.repository.name,
+  //   event.payload.action,
+  //   event.payload.issue.number,
+  //   event.payload.issue.title)
+  console.log(JSON.stringify(event));
+})
+
+// handler.on('note', function (event) {
+// })
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,84 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
+import axios from 'axios';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      trelloConnected: false,
+      boards: [],
+      connected: false,
+      webhooked: false,
+      copied: false,
+      value: ""
+    }
+    this.authenticationSuccess = this.authenticationSuccess.bind(this)
+  }
+  authenticationSuccess(){
+    this.setState({connected: true})
+    let sBoards = this.state.boards
+    let that = this
+    Trello.get("/members/me/boards", { fields: "id" }, function (boards, err) {
+      for (var i = 0; i < boards.length; i++) {
+        Trello.get("/boards/" + boards[i].id, function(board, err){
+          let oBoard = {
+            id: board.id,
+            name: board.name
+          }
+          sBoards.push(oBoard)
+          that.setState({ boards: sBoards })
+        })
+      }
+    })
+  }
+  authenticationFailure(){
+    alert("failed")
+  }
+  handleTrello(e){
+    e.preventDefault()
+    Trello.authorize({
+      type: 'popup',
+      name: 'GitLab to Trello Application',
+      scope: {
+        read: 'true',
+        write: 'true' },
+      expiration: 'never',
+      success: this.authenticationSuccess,
+      error: this.authenticationFailure
+    });
+  }
+  handleWebhook(e){
+    let board_id = e.target.id
+    e.target.disabled = "disabled"
+    let that = this
+    axios.post('/setwebhook', {
+      trelloToken: localStorage.getItem("trello_token"),
+      boardId: board_id
+    }).then((response) => {
+      that.setState({value: response.data})
+    })
+  }
   render() {
     return(
-      <h1>Hello, {this.props.name}!</h1>
+      <div>🌎
+      <button onClick={this.handleTrello.bind(this)} disabled={this.state.connected ? "disabled" : ""}>Connect Trello</button>🌎
+      <input value={this.state.value} disabled="disabled"/>
+      <CopyToClipboard text={this.state.value}
+          onCopy={() => this.setState({copied: true})}>
+      <button>Copy to clipboard</button>
+      </CopyToClipboard>
+      <ul>
+        {this.state.boards.map((board) =>
+          <li key={board.id}>
+            <button id={board.id} onClick={this.handleWebhook.bind(this)}>
+              {board.name}
+            </button>
+          </li>
+        )}
+      </ul>
+      </div>
     )
   }
 };
