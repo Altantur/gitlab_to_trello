25be3640fa55ee1bb1f732d3559b142886e72396
altantur <bayarsaikhan_altantur@unimedia.co.jp>
2017-01-31 19:47:28 +0800

    minor

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.post('/trelloCallback', function(request, response) {
     var action = request.body.action
     var gitlab = null
     var board = boards.find({id: request.body.model.id}).last().value()
+    console.log(board);
     gitlab = board.data.gitlab
     var gitlabAPI = axios.create({
         baseURL: 'http://gitlab.unimedia.mn/api/v3',
